e9097c9a8d5e142dfc61d36bf0c7d351555dd14a
18liedan <148430948+18liedan@users.noreply.github.com>
2026-01-23 11:35:52 +0900

    Update 05_angsdhet.sh

--- 05_angsdhet.sh
+++ 05_angsdhet.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This script performs heterozygosity calculations in ANGSD using each sample's BQSR-calibrated BAM file.
+
 # -------------------------------------------------------------------------
 # SCRIPT CONFIGURATION
 # -------------------------------------------------------------------------
