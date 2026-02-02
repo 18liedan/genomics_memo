4c975de71cb709fd47fd1a32df27e07cbfcab5fe
18liedan <148430948+18liedan@users.noreply.github.com>
2026-02-02 15:49:56 +0900

    Update 04_finalhaplotypecalling.sh

--- 04_finalhaplotypecalling.sh
+++ 04_finalhaplotypecalling.sh
@@ -149,7 +149,7 @@ for SUBSET in "${SUBSETS[@]}"; do
         log "  - SNP Hard-Filtering ($SUBSET)"
         gatk SelectVariants -V "$GENOTYPED_VCF" -select-type SNP -O "${SUBSET_DIR}/temp_snps.vcf.gz"
         gatk VariantFiltration -V "${SUBSET_DIR}/temp_snps.vcf.gz" \
-            -filter "QD < 2.0 || QUAL < 30.0 || SOR > 3.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+            -filter "QD < 2.0 || QUAL < 30.0 || SOR > 3.0 || FS > 60.0 || MQ < 40.0 ||ReadPosRankSum < -8.0" \
             --filter-name "snp_hard_filter" -O "${SUBSET_DIR}/temp_snps_filt.vcf.gz"
         bcftools view -f PASS "${SUBSET_DIR}/temp_snps_filt.vcf.gz" -Oz -o "$SNPS_HARD"
         gatk IndexFeatureFile -I "$SNPS_HARD"
